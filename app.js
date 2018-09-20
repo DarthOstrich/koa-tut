ffec2e2206d67642284f330a5faf8c7340148c09
Jared <jarednutt@outlook.com>
2018-09-20 16:31:51 -0700

    added in error handling

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const router = new Router();
 
 app.use(logger());
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
 router.get('/', (ctx, next) => {
   ctx.body = 'Hello World!';
 });
