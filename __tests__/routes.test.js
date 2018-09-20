bb42be34c46f9d2997b088d588d69ac42dcc7a14
Jared <jarednutt@outlook.com>
2018-09-20 16:20:10 -0700

    tests up and running

---
+++ __tests__/routes.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const server = require('../app.js');
+
+beforeAll(async () => {
+  // do something before anything else runs
+  console.log('Jest starting!');
+});
+// close the server after each test
+afterAll(() => {
+  server.close();
+  console.log('server closed!');
+});
+
+describe('basic route tests', () => {
+  test('get home route  GET /', async () => {
+    const response = await request(server).get('/');
+    expect(response.status).toEqual(200);
+    expect(response.text).toContain('Hello World!');
+  });
+});
