82eeb0989415da3b97fa5feb9b106a9bf691536f
Jared <jarednutt@outlook.com>
2018-09-20 16:13:00 -0700

    end of getting started

---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(logger());
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World!';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
